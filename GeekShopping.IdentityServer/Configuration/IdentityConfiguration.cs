bc75752fb64af588460411b49eb63bb453b2be05
DESKTOP-747UBGO\Enrique <enriquee.flor@gmail.com>
2024-10-09 15:46:08 -0300

    09102024_v2

--- GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
+++ GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -1,52 +1,55 @@
 ﻿using Duende.IdentityServer;
 using Duende.IdentityServer.Models;
+using System.Collections.Generic;
 
 namespace GeekShopping.IdentityServer.Configuration
 {
     public static class IdentityConfiguration
     {
-        public const string Admin = "admin";
-        public const string Client = "customer";
+        public const string Admin = "Admin";
+        public const string Client = "Client";
 
-        public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>
-        {
-            new IdentityResources.OpenId(),
-            new IdentityResources.Email(),
-            new IdentityResources.Profile(),
-        };
-
-        public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>
-        {
-            new ApiScope("geek_shopping", "GeekShopping Server"),
-            new ApiScope(name: "read", "Read data."),
-            new ApiScope(name: "write", "Write data."),
-            new ApiScope(name: "delete", "Delete data."),
-        };
-
-        public static IEnumerable<Client> Clients => new List<Client>
-        {
-            new Client
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
             {
-                ClientId = "client",
-                ClientSecrets = { new Secret("my_super_secret".Sha256())},
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                AllowedScopes = {"read", "write", "profile"}
-            },
-            new Client
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
             {
-                ClientId = "geed_shopping",
-                ClientSecrets = { new Secret("my_super_secret".Sha256())},
-                AllowedGrantTypes = GrantTypes.Code,
-                RedirectUris = {"https://localhost:4430/signin_oidc"},
-                PostLogoutRedirectUris = {"https://localhost:4430/signout-callback_oidc"},
-                AllowedScopes = new List<string>
+                new ApiScope("geek_shopping", "GeekShopping Server"),
+                new ApiScope(name: "read", "Read data."),
+                new ApiScope(name: "write", "Write data."),
+                new ApiScope(name: "delete", "Delete data."),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
                 {
-                    IdentityServerConstants.StandardScopes.OpenId,
-                    IdentityServerConstants.StandardScopes.Profile,
-                    IdentityServerConstants.StandardScopes.Email,
-                    "geek_shopping"
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("my_super_secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"read", "write", "profile" }
+                },
+                new Client
+                {
+                    ClientId = "geek_shopping",
+                    ClientSecrets = { new Secret("my_super_secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = {"https://localhost:4430/signin-oidc"},
+                    PostLogoutRedirectUris = {"https://localhost:4430/signout-callback-oidc"},
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "geek_shopping"
+                    }
                 }
-            }
-        };
+            };
     }
 }
