150d64d70787056021d29df90c26b9060f59ef32
DESKTOP-747UBGO\Enrique <enriquee.flor@gmail.com>
2024-10-30 22:20:37 -0300

    30102024

--- GeekShopping.CartAPI/Controllers/CartController.cs
+++ GeekShopping.CartAPI/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 ﻿using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Messages;
 using GeekShopping.CartAPI.Repository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -68,6 +69,19 @@ namespace GeekShopping.CartAPI.Controllers
             var status = await _repository.RemoveCoupon(userId);
             if (!status) return NotFound();
             return Ok(status);
+        }
+
+        [HttpPost("checkout")]
+        public async Task<IActionResult> Checkout(CheckoutHeaderVO vo)
+        {
+
+            var cart = await _repository.FindCartByUserId(vo.UserId);
+            if (cart == null) return NotFound();
+            vo.CartDetails = cart.CartDetails;
+
+            //TASK RabbitMQ logic comes here!!
+
+            return Ok(vo);
         }
     }
 }
--- GeekShopping.Web/Controllers/CartController.cs
+++ GeekShopping.Web/Controllers/CartController.cs
@@ -59,6 +59,34 @@ namespace GeekShopping.Web.Controllers
             return View();
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Checkout()
+        {
+
+            return View(await FinUserCart());
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Checkout(CartViewModel model)
+        {
+
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var response = await _cartService.Checkout(model.CartHeader, token);
+            if (response != null)
+            {
+                return RedirectToAction(nameof(Confirmation));
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Confirmation()
+        {
+
+            return View();
+        }
+
+
         private async Task<CartViewModel> FinUserCart()
         {
             var token = await HttpContext.GetTokenAsync("access_token");
@@ -74,14 +102,14 @@ namespace GeekShopping.Web.Controllers
                     var coupon = await _couponService.GetCoupon(response.CartHeader.CouponCode, token);
                     if (coupon?.CouponCode != null) 
                     {
-                        response.CartHeader.DiscountTotal = coupon.DiscountAmount;
+                        response.CartHeader.DiscountAmount = coupon.DiscountAmount;
                     }
                 }
                 foreach (var detail in response.CartDetails)
                 {
                     response.CartHeader.PurchaseAmount += (detail.Product.Price * detail.Count);
                 }
-                response.CartHeader.PurchaseAmount -= response.CartHeader.DiscountTotal;
+                response.CartHeader.PurchaseAmount -= response.CartHeader.DiscountAmount;
             }
             return response;
         }
