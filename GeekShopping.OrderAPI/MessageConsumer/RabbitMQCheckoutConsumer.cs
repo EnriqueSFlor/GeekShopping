6eb046ac72107291294fb561cf115058b8f21c22
DESKTOP-747UBGO\Enrique <enriquee.flor@gmail.com>
2024-11-27 18:42:58 -0300

    teste

--- GeekShopping.OrderAPI/MessageConsumer/RabbitMQCheckoutConsumer.cs
+++ GeekShopping.OrderAPI/MessageConsumer/RabbitMQCheckoutConsumer.cs
@@ -1,11 +1,16 @@
-﻿
-using GeekShopping.CartAPI.Repository;
+﻿using GeekShopping.CartAPI.Repository;
 using GeekShopping.OrderAPI.Messages;
 using GeekShopping.OrderAPI.Model;
+using Microsoft.Extensions.Hosting;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace GeekShopping.OrderAPI.MessageConsumer
 {
@@ -22,39 +27,34 @@ namespace GeekShopping.OrderAPI.MessageConsumer
             {
                 HostName = "localhost",
                 UserName = "guest",
-                Password = "guest",
+                Password = "guest"
             };
             _connection = factory.CreateConnection();
-
             _channel = _connection.CreateModel();
-            _channel.QueueDeclare(
-                queue: "checkoutqueue", false, false, false, arguments: null);
+            _channel.QueueDeclare(queue: "checkoutqueue", false, false, false, arguments: null);
         }
-
-
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
             stoppingToken.ThrowIfCancellationRequested();
             var consumer = new EventingBasicConsumer(_channel);
-            consumer.Received += (channel, evt) =>
+            consumer.Received += (chanel, evt) =>
             {
                 var content = Encoding.UTF8.GetString(evt.Body.ToArray());
                 CheckoutHeaderVO vo = JsonSerializer.Deserialize<CheckoutHeaderVO>(content);
                 ProcessOrder(vo).GetAwaiter().GetResult();
                 _channel.BasicAck(evt.DeliveryTag, false);
             };
-
             _channel.BasicConsume("checkoutqueue", false, consumer);
             return Task.CompletedTask;
         }
 
         private async Task ProcessOrder(CheckoutHeaderVO vo)
         {
-            OrderHeader order = new ()
+            OrderHeader order = new()
             {
                 UserId = vo.UserId,
-                FisrtName = vo.FisrtName,
+               // FirstName = vo.FirstName,
                 LastName = vo.LastName,
                 OrderDetails = new List<OrderDetail>(),
                 CardNumber = vo.CardNumber,
@@ -70,7 +70,7 @@ namespace GeekShopping.OrderAPI.MessageConsumer
                 DateTime = vo.DateTime
             };
 
-            foreach(var details in vo.CartDetails)
+            foreach (var details in vo.CartDetails)
             {
                 OrderDetail detail = new()
                 {
@@ -78,11 +78,11 @@ namespace GeekShopping.OrderAPI.MessageConsumer
                     ProductName = details.Product.Name,
                     Price = details.Product.Price,
                     Count = details.Count,
-
                 };
                 order.CartTotalItens += details.Count;
                 order.OrderDetails.Add(detail);
             }
+
             await _repository.AddOrder(order);
         }
     }
--- GeekShopping.OrderAPI/Program.cs
+++ GeekShopping.OrderAPI/Program.cs
@@ -1,102 +1,26 @@
-using GeekShopping.CartAPI.Repository;
-using GeekShopping.OrderAPI.MessageConsumer;
-using GeekShopping.OrderAPI.Model.Context;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+namespace GeekShopping.OrderAPI
 {
-    c.EnableAnnotations();
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    public class Program
     {
-        Description = @"Enter 'Bearer' [space] and your token!",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-    {
-        new OpenApiSecurityScheme
+        public static void Main(string[] args)
         {
-            Reference = new OpenApiReference
-            {
-                Type = ReferenceType.SecurityScheme,
-                Id = "Bearer"
-            },
-            Scheme = "oauth2",
-            Name = "Bearer",
-            In = ParameterLocation.Header
-        },
-        new List<string>()
-    }});
+            CreateHostBuilder(args).Build().Run();
+        }
 
-});
-
-
-var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
-
-builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 38))));
-
-var builde = new DbContextOptionsBuilder<MySQLContext>();
-builde.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 38)));
-
-builder.Services.AddSingleton(new OrderRepository(builde.Options));
-
-builder.Services.AddHostedService<RabbitMQCheckoutConsumer>();
-
-
-builder.Services.AddControllers();
-
-builder.Services.AddAuthentication("Bearer")
-    .AddJwtBearer("Bearer", options =>
-    {
-        options.Authority = "https://localhost:4435/";
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateAudience = false
-        };
-        options.RequireHttpsMetadata = false; ///
-    });
-
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("ApiScope", policy =>
-    {
-        policy.RequireAuthenticatedUser();
-        policy.RequireClaim("scope", "geek_shopping");
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
-    );
-
-});
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
